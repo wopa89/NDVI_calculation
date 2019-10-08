0f7cdfa7113e22f98739c588b14469fa2994355e
wopa89 <44268954+wopa89@users.noreply.github.com>
2019-10-08 16:45:22 +0200

    added ndvi calculation at two different points in time and calculation of the difference

--- ndvi.py
+++ ndvi.py
@@ -20,58 +20,116 @@ def calc_ndvi(nir,red):
     return ndvi
 
 #search and download satellite images
-search = Search(bbox=[9.842292,52.868683, 10.336440,52.604698],
+search1 = Search(bbox=[9.842292,52.868683, 10.336440,52.604698],
                datetime='2018-08-06',
                property=["eo:cloud_cover<5"])
-items = search.items()
+items1 = search1.items()
+search2 = Search(bbox=[9.842292,52.868683, 10.336440,52.604698],
+               datetime='2013-08-24',
+               property=["eo:cloud_cover<5"])
+items2 = search2.items()
 
 #Loop through the items list and search for landsat scene
 #   as sentinel scenes throws an error message
-for item in items:
+for item in items1:
     item_prop = item.properties["collection"]
     if item_prop.startswith("landsat"):
-        item_band4_url = item.assets["B4"]["href"]    #red
-        item_band5_url = item.assets["B5"]["href"]    #NIR
+        item1_band4_url = item.assets["B4"]["href"]    #red
+        item1_band5_url = item.assets["B5"]["href"]    #NIR
+        break
+    else:
+        continue
+for item in items2:
+    item_prop = item.properties["collection"]
+    if item_prop.startswith("landsat"):
+        item2_band4_url = item.assets["B4"]["href"]    #red
+        item2_band5_url = item.assets["B5"]["href"]    #NIR
         break
     else:
         continue
 
 #Check if landsat scene was found, otherwise stop here
 try:
-    item_found = item_band4_url[0]
+    item1_found = item1_band4_url[0]
+    item2_found = item2_band4_url[0]
 except:
     print("No landsat scene found")
     sys.exit()
 
+#hier kommt funktion für tiling
+
 #create bounding box and convert geographic coordinates to image coordinates
-with rio.open(item_band4_url) as src:
-    affine = src.transform
-    print(affine)
-    xmin = affine[2] + 45000
-    xmax = affine[2] + 65000
-    ymin = affine[5] - 50000
-    ymax = affine[5] - 30000
-    col_start, row_start = ~affine * (xmin, ymax)
-    col_end, row_end = ~affine * (xmax, ymin)
-    print(col_start, row_start)
-    print(col_end, row_end)
+with rio.open(item1_band4_url) as src:
+    affine1 = src.transform
+    print(affine1)
+    xmin1 = affine1[2] + 45000
+    xmax1 = affine1[2] + 65000
+    ymin1 = affine1[5] - 50000
+    ymax1 = affine1[5] - 30000
+    col_start1, row_start1 = ~affine1 * (xmin1, ymax1)
+    col_end1, row_end1 = ~affine1 * (xmax1, ymin1)
+#create bounding box and convert geographic coordinates to image coordinates
+with rio.open(item2_band4_url) as src:
+    affine2 = src.transform
+    print(affine2)
+    xmin2 = affine1[2] + 45000
+    xmax2 = affine1[2] + 65000
+    ymin2 = affine1[5] - 50000
+    ymax2 = affine1[5] - 30000
+    col_start2, row_start2 = ~affine2 * (xmin2, ymax2)
+    col_end2, row_end2 = ~affine2 * (xmax2, ymin2)
 
 #create window with image coordinates
-with rio.open(item_band4_url) as src:
-    band4 = src.read(1, window=((int(row_start), int(row_end)), (int(col_start), int(col_end)) )) #
-    
-with rio.open(item_band5_url) as src:
-    band5 = src.read(1, window=((int(row_start), int(row_end)), (int(col_start), int(col_end)) )) #
+with rio.open(item1_band4_url) as src:
+    item1_band4 = src.read(1, window=((int(row_start1), int(row_end1)), (int(col_start1), int(col_end1)) )) #
+with rio.open(item1_band5_url) as src:
+    item1_band5 = src.read(1, window=((int(row_start1), int(row_end1)), (int(col_start1), int(col_end1)) )) #
+#create window with image coordinates
+with rio.open(item2_band4_url) as src:
+    item2_band4 = src.read(1, window=((int(row_start2), int(row_end2)), (int(col_start2), int(col_end2)) )) #
+with rio.open(item2_band5_url) as src:
+    item2_band5 = src.read(1, window=((int(row_start2), int(row_end2)), (int(col_start2), int(col_end2)) ))
+
     
 #allow division by zero
 numpy.seterr(divide='ignore', invalid='ignore')
 
 #call function
-ndvi = calc_ndvi(band5,band4)
+ndvi1 = calc_ndvi(item1_band5,item1_band4)
+ndvi2 = calc_ndvi(item2_band5,item2_band4)
 
+#calculate difference
+ndvi = ndvi1 - ndvi2
+
+#plot rasters
+fig, axes = plt.subplots(1,3, figsize=(14,6), sharex=True, sharey=True)
+date1 = '2018-08-06'
+date2 = '2013-08-24'
+
+#plot ndvi date1
+plt.sca(axes[0])
+plt.imshow(ndvi2, cmap='RdYlGn', vmin=-1, vmax=1)
+plt.colorbar(shrink=0.5)
+plt.title('NDVI {}'.format(date2))
+plt.xlabel('Column #')
+plt.ylabel('Row #')
+
+#plot ndvi date2
+plt.sca(axes[1])
+plt.imshow(ndvi1, cmap='RdYlGn', vmin=-1, vmax=1)
+plt.colorbar(shrink=0.5)
+plt.title('NDVI {}'.format(date1))
+
+#plot difference
+plt.sca(axes[2])
+plt.imshow(ndvi, cmap='bwr', vmin=-1, vmax=1)
+plt.colorbar(shrink=0.5)
+plt.title('Diff ({} - {})'.format(date1, date2))
+
+#write raster
 #get x,y pixel size
-x = band4.shape[0]
-y = band4.shape[1]
+x = item1_band4.shape[0]
+y = item1_band4.shape[1]
 
 localname = 'example.tif'
 #Zuerst wird das Raster erstellt mit driver, width, height, count und dtype
@@ -79,15 +137,3 @@ localname = 'example.tif'
 #   In dem Fall soll das Band so groß sein wie das geschriebene Raster
 with rio.open(localname, 'w', driver='GTiff', width=x, height=y, count=1, dtype=ndvi.dtype) as dst:
     dst.write(ndvi, window = rio.windows.Window(0,0, x,y), indexes=1)
-
-#reopen and plotting raster
-datetime='2018-08-06'
-with rio.open(localname) as src:
-    print(src.profile)
-    ndvi = src.read(1) # read the entire array
-
-plt.imshow(ndvi, cmap='RdYlGn')
-plt.colorbar()
-plt.title('Overview - Pixel {}'.format(band4.shape))
-plt.xlabel('Column #')
-plt.ylabel('Row #')
